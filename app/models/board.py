03f49d9a58907a3c651e9cd54965bebc3cd92da0
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-26 13:28:53 -0700

    added board model

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+    card = db.relationship('Card', backref='board')
