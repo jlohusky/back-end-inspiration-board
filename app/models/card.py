c3e9b732c25ad0e4a47f63ce702467396b1575b8
Angie <caritocon@gmail.com>
2023-06-26 13:15:27 -0700

    Card model finished

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,10 @@
 from app import db
+from app.models.board import Board
+
+class Card(db.Model):
+
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+
