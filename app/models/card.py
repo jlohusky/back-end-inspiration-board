fdb9b7b95e35d041581dcfcb5584371c1ab350ea
Angie <caritocon@gmail.com>
2023-06-26 15:16:30 -0700

    Card models and routes

--- app/models/card.py
+++ app/models/card.py
@@ -8,3 +8,10 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
 
+    def to_dict(self):
+        return \
+            {
+                'id': self.card_id,
+                'message': self.message,
+                'likes_count': self.likes_count
+            }
