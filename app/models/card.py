69c0e0ffb0f37e93b2f0bf50d2d6a2bfab432d46
Angie <caritocon@gmail.com>
2023-06-27 11:17:29 -0700

    Added default to likes count column

--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ class Card(db.Model):
 
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
     def to_dict(self):
         return \
