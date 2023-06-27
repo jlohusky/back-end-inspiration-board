15a92296f5c80fb05ef91ba14a0959d18a12721d
Jennifer Tam <jenniferwtam@gmail.com>
2023-06-27 10:31:35 -0700

    ran flask db migrate

--- app/models/card.py
+++ app/models/card.py
@@ -8,26 +8,11 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
 
-<<<<<<< HEAD
-    @classmethod
-    def dict_for_post_method(cls, cards_details):
-
-        result = cls(
-            message=cards_details["message"],
-            likes_count=0)
-        return result
-    
-=======
->>>>>>> 82e662be0fe1264ae0736a9bad64c5d1a6857342
     def to_dict(self):
         return \
             {
                 'id': self.card_id,
                 'message': self.message,
-<<<<<<< HEAD
-                'likes_count': self.likes_count
-=======
                 'likes_count': self.likes_count,
                 'board_id': self.board_id
->>>>>>> 82e662be0fe1264ae0736a9bad64c5d1a6857342
             }
